356ff34c4b974353c191875f52eba3a2a4b50b27
Cosmic Soul <SuneelKshetri@gmail.com>
2023-08-25 18:41:12 +0545

    new topic slicing added

---
+++ slicing.py
@@ -0,0 +1,18 @@
+#python list
+piano_keys = ["a", "b" , "c", "d" , "e", "f", "g", "h"]
+
+
+print(piano_keys[2:5])
+print (piano_keys[:6])
+
+#skip 2nd position
+print(piano_keys[::2])
+
+#reverse the list
+print(piano_keys[::-1])
+
+
+#same method works for python tuple
+
+#python tuple
+piano_tuple = ("a", "b" , "c", "d" , "e", "f", "g", "h")
